0c4f20aac6f2903127f427822e922b894166d4ae
Shmuel Arditi <shmuel.arditi@develeap.com>
2021-12-09 01:56:48 +0200

    changes to jenkins file

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,76 +1,83 @@
+# module "jenkins" {
+#   source = "terraform-module/release/helm"
+
+#   namespace  = "default"
+#   repository = "https://charts.jenkins.io"
+
+#   app = {
+#     name         = "jenkins"
+#     version      = "3.9.3"
+#     chart        = "jenkins"
+#     force_update = true
+#     wait         = false
+#     deploy       = 1
+#   }
+
+#   set = [
+#     {
+#       name  = "controller.serviceType"
+#       value = "LoadBalancer"
+#     },
+#     {
+#       name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+#       value = "nlb"
+#     },
+#     {
+#       name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+#       value = "internal"
+#     },
+#     {
+#       name  = "agent.tag"
+#       value = "latest"
+#     },
+#     {
+#       name  = "agent.iamge"
+#       value = "ubuntu"
+#     }
+#   ]
+
+#   set_sensitive = [
+#     {
+#       path  = "controller.adminUser"
+#       value = "jenkins"
+#     }
+#   ]
+# }
+
 module "jenkins" {
-  source = "terraform-module/release/helm"
-
-  namespace  = "default"
-  repository = "https://charts.jenkins.io"
-
-  app = {
-    name         = "jenkins"
-    version      = "3.9.3"
-    chart        = "jenkins"
-    force_update = true
-    wait         = false
-    deploy       = 1
-  }
-
-  set = [
-    {
-      name  = "controller.serviceType"
-      value = "LoadBalancer"
-    },
-    {
-      name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "nlb"
-    },
-    {
-      name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-      value = "internal"
-    },
-    {
-      name  = "agent.tag"
-      value = "latest"
-    },
-    {
-      name  = "agent.iamge"
-      value = "ubuntu"
-    }
-  ]
-
-  set_sensitive = [
-    {
-      path  = "controller.adminUser"
-      value = "jenkins"
-    }
-  ]
+  source  = "./modules/jenkins/"
+  # insert the 1 required variable here
+  create_namespace = true
+  service_type = "LoadBalancer"
 }
 
-module "helloworld" {
-  source = "terraform-module/release/helm"
+# module helloworld" {
+#   source = "terraform-module/release/helm"
 
-  namespace  = "default"
-  repository = "https://helmcharts.opsmx.com/"
+#   namespace  = "default"
+#   repository = "https://helmcharts.opsmx.com/"
 
-  app = {
-    name         = "hello-world"
-    version      = "1.0.3"
-    chart        = "hello-kubernetes"
-    force_update = true
-    wait         = false
-    deploy       = 1
-  }
+#   app = {
+#     name         = "hello-world"
+#     version      = "1.0.3"
+#     chart        = "hello-kubernetes"
+#     force_update = true
+#     wait         = false
+#     deploy       = 1
+#   }
 
-  set = [
-    # {
-    #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-    #   value = "nlb"
-    # },
-    # {
-    #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-    #   value = "internal"
-    # },
-    # {
-    #   name  = "serviceType"
-    #   value = "LoadBalancer"
-    # }
-  ]
-}
+#   set = [
+#     # {
+#     #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+#     #   value = "nlb"
+#     # },
+#     # {
+#     #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+#     #   value = "internal"
+#     # },
+#     # {
+#     #   name  = "serviceType"
+#     #   value = "LoadBalancer"
+#     # }
+#   ]
+# }
