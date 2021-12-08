7cd9dffc277d8083eee96907c3a7acb0453a4288
Shmuel Arditi <shmuel.arditi@develeap.com>
2021-12-08 20:19:05 +0200

    changes to jenkinsfile

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -35,3 +35,34 @@ module "jenkins" {
     }
   ]
 }
+
+module "helloworld" {
+  source = "terraform-module/release/helm"
+
+  namespace  = "default"
+  repository = "https://helmcharts.opsmx.com/"
+
+  app = {
+    name         = "hello-world"
+    version      = "1.0.3"
+    chart        = "hello-kubernetes"
+    force_update = true
+    wait         = false
+    deploy       = 1
+  }
+
+  set = [
+    # {
+    #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    #   value = "nlb"
+    # },
+    # {
+    #   name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    #   value = "internal"
+    # },
+    # {
+    #   name  = "serviceType"
+    #   value = "LoadBalancer"
+    # }
+  ]
+}
