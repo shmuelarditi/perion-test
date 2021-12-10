357aa68d77203db3641a5d5848a16168ae47578b
Shmuel Arditi <shmuel.arditi@develeap.com>
2021-12-10 14:49:58 +0200

    jenkinsfile testing

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,55 +1,48 @@
-# module "jenkins" {
-#   source = "terraform-module/release/helm"
-
-#   namespace  = "default"
-#   repository = "https://charts.jenkins.io"
-
-#   app = {
-#     name         = "jenkins"
-#     version      = "3.9.3"
-#     chart        = "jenkins"
-#     force_update = true
-#     wait         = false
-#     deploy       = 1
-#   }
-
-#   set = [
-#     {
-#       name  = "controller.serviceType"
-#       value = "LoadBalancer"
-#     },
-#     {
-#       name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-#       value = "nlb"
-#     },
-#     {
-#       name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-#       value = "internal"
-#     },
-#     {
-#       name  = "agent.tag"
-#       value = "latest"
-#     },
-#     {
-#       name  = "agent.iamge"
-#       value = "ubuntu"
-#     }
-#   ]
-
-#   set_sensitive = [
-#     {
-#       path  = "controller.adminUser"
-#       value = "jenkins"
-#     }
-#   ]
-# }
-
 module "jenkins" {
-  source  = "./modules/jenkins/"
-  # insert the 1 required variable here
-  create_namespace = true
-  service_type = "LoadBalancer"
+  source = "terraform-module/release/helm"
+
+  namespace  = "default"
+  repository = "https://charts.jenkins.io"
+
+  app = {
+    name         = "jenkins"
+    version      = "3.9.3"
+    chart        = "jenkins"
+    force_update = true
+    wait         = false
+    deploy       = 1
+  }
+
+  set = [
+    {
+      name  = "controller.serviceType"
+      value = "LoadBalancer"
+    },
+    {
+      name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+    },
+    {
+      name  = "controller.serviceAnnotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+      value = "internal"
+    }
+  ]
+
+  set_sensitive = [
+    {
+      path  = "controller.adminUser"
+      value = "jenkins"
+    }
+  ]
 }
+
+
+# module "jenkins" {
+#   source  = "./modules/jenkins/"
+#   # insert the 1 required variable here
+#   create_namespace = true
+#   service_type = "LoadBalancer"
+# }
 
 # module helloworld" {
 #   source = "terraform-module/release/helm"
